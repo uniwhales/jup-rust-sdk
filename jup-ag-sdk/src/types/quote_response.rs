a41f2c563e9accbc6d91e39a2abea62cd4eab25f
Timur M <timurterminti@gmail.com>
2025-12-24 11:47:44 +0000

    skip serializing fee_amount and fee_mint when they're None

--- jup-ag-sdk/src/types/quote_response.rs
+++ jup-ag-sdk/src/types/quote_response.rs
@@ -91,9 +91,9 @@ pub struct SwapInfo {
     pub output_mint: String,
     pub in_amount: String,
     pub out_amount: String,
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub fee_amount: Option<String>,
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub fee_mint: Option<String>,
 }
 
